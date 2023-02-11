d9e79f4701903e2e82fb70e8593aa888522b8f67
Jitendra Kumar Sahu <jitendra.logistifie@gmail.com>
2023-02-11 20:02:43 +0530

    home screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/screen/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      //title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-     // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+       home: Home(),
     );
   }
 }
